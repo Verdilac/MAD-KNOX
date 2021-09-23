4e60350f1baac4eb40171b098db1c33b49521a03
lathindu <64456879+Verdilac@users.noreply.github.com>
2021-09-24 00:36:08 +0530

    added

--- app/src/main/java/com/example/mad_knox/MainActivity.java
+++ app/src/main/java/com/example/mad_knox/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         //INITILIZIATION
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
