d77dbfc82a5552083b634341093f2118eabdd690
lathindu <64456879+Verdilac@users.noreply.github.com>
2021-09-23 22:04:47 +0530

    Initiliziation

---
+++ app/src/main/java/com/example/mad_knox/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.mad_knox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        //INITILIZIATION
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
