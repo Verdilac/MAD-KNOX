978276d292aed9daeebdc9975ebb492e41e53dc2
lathindu <64456879+Verdilac@users.noreply.github.com>
2021-09-25 02:46:21 +0530

    Ui setup refinement

--- app/src/main/java/com/example/mad_knox/MainActivity.java
+++ app/src/main/java/com/example/mad_knox/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.mad_knox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    Button buttonOpenSecond;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +19,19 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        buttonOpenSecond = (Button) findViewById(R.id.buttonOpenSecond);
+        buttonOpenSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,PaymentActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+
 }
