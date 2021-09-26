fc3a1bb316d3ce803cbd07368355a021309a15e1
ushanT98 <ushanthimirapc@gmail.com>
2021-09-26 17:24:52 +0530

    Delivery

---
+++ app/src/main/java/com/example/mad_knox/DeliveryCRUD.java
@@ -0,0 +1,14 @@
+package com.example.mad_knox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeliveryCRUD extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delivery_crud);
+    }
+}
