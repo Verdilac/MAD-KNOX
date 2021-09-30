18320b3a72e55b888fd44dd380875edb3416549d
KulajaKarunaratne <it20393748@my.sliit.lk>
2021-09-30 22:22:31 +0530

    Push on Sep30 10.22

---
+++ app/src/main/java/com/example/mad_knox/UserProfile_k.java
@@ -0,0 +1,14 @@
+package com.example.mad_knox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserProfile_k extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile_k);
+    }
+}
