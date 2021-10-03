87a75ccce8dd84483e498abf44896d61217239e4
KulajaKarunaratne <it20393748@my.sliit.lk>
2021-10-03 11:33:27 +0530

    Push on Oct 3 11.33am

--- app/src/main/java/com/example/mad_knox/SellersOutlet_k.java
+++ app/src/main/java/com/example/mad_knox/SellersOutlet_k.java
@@ -3,6 +3,8 @@ package com.example.mad_knox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SellersOutlet_k extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class SellersOutlet_k extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sellers_outlet_k);
+        EditText edit_product_name = findViewById(R.id.edit_product_name);
+        EditText edit_product_category = findViewById(R.id.edit_product_category);
+        EditText edit_product_price = findViewById(R.id.edit_product_price);
+        EditText edit_product_quantity = findViewById(R.id.edit_product_quantity);
+        EditText edit_product_description = findViewById(R.id.edit_product_description);
+        EditText edit_product_image_link = findViewById(R.id.edit_product_image_link);
+        Button btn = findViewById(R.id.btn_add_information);
+        btn.setOnClickListener(v->
+        {
+
+
+
+        });
     }
 }
