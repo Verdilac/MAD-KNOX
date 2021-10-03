e94d014d562b30c70d8ba184285f191215ca4afa
KulajaKarunaratne <it20393748@my.sliit.lk>
2021-10-03 11:42:06 +0530

    Push on Oct4 11.41am

--- app/src/main/java/com/example/mad_knox/ProductListing.java
+++ app/src/main/java/com/example/mad_knox/ProductListing.java
@@ -1,5 +1,19 @@
 package com.example.mad_knox;
 
 public class ProductListing {
-    
+    private String product_name;
+    private String product_category;
+    private String product_price;
+    private String product_quantity;
+    private String product_description;
+    private String product_image_link;
+
+    public ProductListing(String product_name, String product_category, String product_price, String product_quantity, String product_description, String product_image_link) {
+        this.product_name = product_name;
+        this.product_category = product_category;
+        this.product_price = product_price;
+        this.product_quantity = product_quantity;
+        this.product_description = product_description;
+        this.product_image_link = product_image_link;
+    }
 }
