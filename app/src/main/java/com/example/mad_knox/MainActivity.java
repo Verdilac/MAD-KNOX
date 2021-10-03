0c2e057b854ae74e42bf0020f2f94bc32be7bbe8
lathindu <64456879+Verdilac@users.noreply.github.com>
2021-10-03 23:16:40 +0530

    final start

--- app/src/main/java/com/example/mad_knox/MainActivity.java
+++ app/src/main/java/com/example/mad_knox/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
         //INITILIZIATION
 
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
