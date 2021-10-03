34bc96509e5c273cb927936260f3e1a56bdcb727
KulajaKarunaratne <it20393748@my.sliit.lk>
2021-10-03 08:28:48 +0530

    Push on Oct3 8.28am

---
+++ app/src/main/java/com/example/mad_knox/AddProductListing_k.java
@@ -0,0 +1,14 @@
+package com.example.mad_knox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddProductListing_k extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_product_listing_k);
+    }
+}
